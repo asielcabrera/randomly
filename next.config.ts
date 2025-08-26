a99e4d6bd6b6f660665cc8d04ad018fe4caad711
Asiel Cabrera <cabrerasiel@gmail.com>
2025-08-26 15:48:29 -0400

    adding netlify comfig

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+    images: { unoptimized: true }, // quick fix; you can re-enable later
+  reactStrictMode: true,
   /* config options here */
 };
 
