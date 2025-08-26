e37826e7eda22e267c942a0e61ac1b34e99880af
Asiel Cabrera <cabrerasiel@gmail.com>
2025-08-26 15:44:49 -0400

    removing google font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,7 @@
 
 import "./globals.css";
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import { SiteHeader } from "@/components/site-header";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Tropi Coqueta Networking Party Selector",
